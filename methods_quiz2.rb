ab6cd2d50ce5ae35b8b44670c0193c214a830e01
Joshua Madden <jmaddenco@gmail.com>
2015-09-11 10:16:17 -0600

    This was hard for me, do you think you could give me some sights or information that I could use to better understand what to do?

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,5 +1,25 @@
-module MethodsQuiz
+module MethodsQuiz2
 	
-	
+
+	def without_doubles? side_1, side_2, no_doubles
+		if (side_1 + side_2) && no_doubles == false then
+			return (side_1 + side_2)
+				elsif no_doubles == true then
+					return ((side_1 + 1) + side_2)
+		end
+
+	end
+
+	private
+
+
+
+		def no_doubles
+			if side_1 != side_2 then
+				return false
+			end
+
+		end
+
 
 end
--- methods_quiz2_test.rb
+++ methods_quiz2_test.rb
@@ -12,5 +12,13 @@ class MethodsQuizTest < MiniTest::Test
 	end
 
 	# TODO - write tests here
+	def test_without_doubles
+		assert_equal 8, @m.without_doubles?(3, 5, false)
+		assert_equal 7, @m.without_doubles?(2, 5, false)
 
+		assert_equal 11, @m.without_doubles?(5, 5, true)
+		assert_equal 7, @m.without_doubles?(3, 3, true)
+
+#		assert_equal 2, @m.without_doubles?(6, 6, true)
+	end
 end
