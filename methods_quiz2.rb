01f39e8b08d8a814d08e547336e4f556d50283cd
Joshua Madden <jmaddenco@gmail.com>
2015-09-14 10:16:35 -0600

    finished test side and code side of red_ticket

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,25 +1,53 @@
 module MethodsQuiz2
 	
-
-	def without_doubles? side_1, side_2, no_doubles
-		if (side_1 + side_2) && no_doubles == false then
-			return (side_1 + side_2)
-				elsif no_doubles == true then
-					return ((side_1 + 1) + side_2)
+		def without_doubles num_1, num_2, no_doubles
+			roll_back?(num_1, num_2, no_doubles) ? num_1 = 1 : num_1
+			no_doubles?(no_doubles) ? (num_1 == num_2 ? num_1 + 1 + num_2 : num_1 + num_2) : num_1 + num_2
 		end
 
-	end
+		def max_maybe num_1, num_2
+			equal?(num_1, num_2) ? 0 : (same_remainder?(num_1, num_2) ? (num_1_greater?(num_1, num_2) ? num_2 : num_1) : (num_1_greater?(num_1, num_2) ? num_1 : num_2))
+		end
 
-	private
+		def squirrels_play? temp, summer
+			summer?(summer) ? temp >= 60 && temp <= 100 : temp >= 60 && temp <= 90
+		end
 
+		def red_ticket a, b, c
+			all_same?(a, b, c) ? (all_2?(a, b, c) ? 10 : 5) : (bc_different?(a, b, c) ? 1 : 0)
+		end
 
-
-		def no_doubles
-			if side_1 != side_2 then
-				return false
+		private
+			def no_doubles? x
+				x ? true : false
+			end
+			def roll_back? a, b, x
+				a == 6 && b == 6 && x == true ? true : false
 			end
 
-		end
+			def equal? a, b
+				a == b ? true : false
+			end
+			def num_1_greater? a, b
+				a > b ? true : false
+			end
+			def same_remainder? a, b
+				a % 5 == b % 5 ? true : false
+			end
 
-
+			def summer? x
+				x ? true : false
+			end
+			def all_same? a, b, c
+				a == b && b == c ? true : false
+			end
+			def bc_different? a, b, c
+				a != c ? true : false				
+			end
+			def all_2? a, b, c
+				a == 2 && b == 2 &&  c == 2 ? true : false
+			end
+			def all_different? a, b ,c
+				a != b && b != c ? true : false
+			end
 end
